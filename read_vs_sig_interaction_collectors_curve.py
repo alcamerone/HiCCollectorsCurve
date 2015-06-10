fb100153f98317feb88e679cb1938ad5f477617f
Cameron Ekblad <cekb635@aucklanduni.ac.nz>
2015-06-10 16:53:31 +1200

    20150610-Working, no multiprocessing commit.

--- read_vs_sig_interaction_collectors_curve.py
+++ read_vs_sig_interaction_collectors_curve.py
@@ -1,5 +1,6 @@
 import argparse
 import random
+import os
 from sets import Set
 
 def main(args):
@@ -33,15 +34,17 @@ def main(args):
 	
 	#Identify interactions as significant
 	sig_ints = open(args.sig_ints_fp, 'r')
+	num_sig_ints = 0
 	print "Identifying significant interactions..."
 	sig_ints.readline()
-	for i,line in enumerate(sig_ints):
+	for line in sig_ints:
 		sig_int = line.strip().split('\t')
 		probe1 = sig_int[0]
 		probe2 = sig_int[4]
 		if probe_index.has_key(probe1) and probe_index.has_key(probe2):
 			sig_matrix[probe_index[probe1]][probe_index[probe2]] = True #If an interaction between probe 1 and probe 2 appears, set the corresponding value in the matrix to True
 			sig_matrix[probe_index[probe2]][probe_index[probe1]] = True #Do this for probe 2 and probe 1 value as well
+			num_sig_ints += 1
 	sig_ints.close()
 	
 	sam_file = open(args.sam_file_fp, 'r')
@@ -51,14 +54,22 @@ def main(args):
 	reads = sam_file.readlines()
 	reads.insert(0,line)
 	results = []
-	for size in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
-		results.append(subsample_and_compare(sig_matrix,reads,size,len(probe_index.keys()),chr_index,chr_bins))
+	ss_sizes = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+	print "Performing subsampling..."
+	for size in ss_sizes:
+		subsample_sam(reads,size,args.num_iter,args.sam_file_fp[:args.sam_file_fp.rfind('/')+1] + "tmp/")
+	print "Processing subsamples..."
+	proportion_sig_ints = process_subsamples(args.sam_file_fp[:args.sam_file_fp.rfind('/')+1] + "tmp/",ss_sizes,chr_bins,chr_index,sig_matrix,num_sig_ints,probe_list)
+	mean_proportion_sig_ints = {}
+	for key in proportion_sig_ints:
+		mean_proportion_sig_ints[key] = sum(proportion_sig_ints[key])/float(args.num_iter)
+	print mean_proportion_sig_ints
 	
 	"""for chr in chr_list:
 		print chr + ": "
 		for index,bin in enumerate(chr_bins[chr]):
 			print "Bin " + bin + " links to probe " + probe_list[chr_index[chr]+index]
-		
+	"""	
 	sig_matrix_test = open("sig_matrix_test.txt",'w')
 	print "Writing test file..."
 	for probe in probe_list:
@@ -67,11 +78,73 @@ def main(args):
 	for row in range(0,len(sig_matrix)):
 		sig_matrix_test.write(probe_list[row])
 		for col in sig_matrix[row]:
-			sig_matrix_test.write(str(col) + '\t')
+			sig_matrix_test.write('\t' + str(col))
 		sig_matrix_test.write('\n')
-	sig_matrix_test.close()"""
+	sig_matrix_test.close()
+	
+def subsample_sam(reads,size,num_times,temp_dir): #num_times: the number of times to subsample the SAM file
+	if not os.path.exists(temp_dir):
+		print "Creating directory..."
+		os.mkdir(temp_dir)
+		print "Directory created."
+	temp_dir = temp_dir + str(size) + '/' #Include a subdirectory for each group of subsamples
+	if not os.path.exists(temp_dir):
+		print "Creating directory..."
+		os.mkdir(temp_dir)
+		print "Directory created."
+	num_runs = int(size*len(reads)) #The number of lines to pull from the SAM file
+	for i in range(int(num_times)):
+		seen = Set([]) #Keeps track of which lines have been seen
+		curr_run = 0
+		file_out = open(temp_dir + str(size) + "_subsample_" + str(i) + ".txt", 'w')
+		while curr_run < num_runs:
+			line_num = random.randint(0,len(reads)-1)
+			if not line_num in seen:
+				seen.add(line_num)
+				file_out.write(reads[line_num])
+				curr_run += 1
+		file_out.close()
+		
+def process_subsamples(temp_dir,sizes,chr_bins,chr_index,sig_matrix,sig_ints,probe_list):
+	proportion_sig_ints = {} #Keeps track of the proportions for each iteration of each subsample size
+	for size in sizes:
+		proportion_sig_ints[size] = []
+	for dir in os.listdir(temp_dir):
+		size = float(dir)
+		print "Processing " + dir + "..."
+		for file in os.listdir(temp_dir + '/' + dir):
+			print "Processing " + file + "..."
+			hit = Set([]) #Keeps track of which significant interactions have been seen
+			sample = open(temp_dir + '/' + dir + '/' + file,'r')
+			for line in sample:
+				read = line.split('\t')
+				chr1 = "Chr" + read[2]
+				pos1 = read[3]
+				if read[6] == '=':
+					chr2 = chr1
+				else:
+					chr2 = "Chr" + read[6]
+				pos2 = read[7]
+				if (chr_bins.has_key(chr1) and chr_bins.has_key(chr2)) and (chr_index.has_key(chr1) and chr_index.has_key(chr2)):
+					indices1 = search_bins(chr_bins[chr1],0,len(chr_bins[chr1])-1,pos1)
+					indices2 = search_bins(chr_bins[chr2],0,len(chr_bins[chr1])-1,pos2)
+					for index1 in indices1:
+						index1 += chr_index[chr1]
+						for index2 in indices2:
+							index2 += chr_index[chr2]
+							if not str(index1) + ',' + str(index2) in hit:
+								if sig_matrix[index1][index2]:
+									hit.add(str(index1) + ',' + str(index2))
+									hit.add(str(index2) + ',' + str(index1)) #As this is equivalent
+			proportion_sig_ints[size].append((float(len(hit))/2.0)/float(sig_ints)) #Divide the number of hit significant interactions by two, as they were added "twice" to the set, then divide by the total number of significant interactions to get the proportion of significant interactions hit by the reads in this subsample"""
+			print hit
+			print "Hits:"
+			for tuple in hit:
+				print probe_list[int(tuple.split(',')[0])] + " " + probe_list[int(tuple.split(',')[1])]
+			sample.close()
+	return proportion_sig_ints
 
-def subsample_and_compare(sig_matrix,sam_reads,size,sig_ints,chr_index,chr_bins):
+"""def subsample_and_compare(sig_matrix,sam_reads,size,sig_ints,chr_index,chr_bins):
 	seen = Set([]) #Keeps track of which lines have been seen
 	hit = Set([]) #Keeps track of which significant interactions have been seen
 	num_runs = int(len(sam_reads)*size)
@@ -87,19 +160,21 @@ def subsample_and_compare(sig_matrix,sam_reads,size,sig_ints,chr_index,chr_bins)
 			else:
 				chr2 = "Chr" + read[6]
 			pos2 = read[7]
-			indices1 = chr_index[chr1] + search_bins(chr_bins[chr1],0,len(chr_bins[chr1])-1,pos1)
-			indices2 = chr_index[chr2] + search_bins(chr_bins[chr2],pos2)
+			indices1 = search_bins(chr_bins[chr1],0,len(chr_bins[chr1])-1,pos1)
+			indices2 = search_bins(chr_bins[chr2],pos2)
 			for index1 in indices1:
+				index1 += chr_index[chr1]
 				for index2 in indices2:
+					index2 += chr_index[chr2]
 					if not str(index1) + ',' + str(index2) in hit:
 						if sig_matrix[index1][index2]:
 							hit.add(str(index1) + ',' + str(index2))
 							hit.add(str(index2) + ',' + str(index1)) #As this is equivalent
 			num_runs -= 1
-	return float(hit/2)/float(sig_ints) #Divide the number of hit significant interactions by two, as they were added "twice" to the set, then divide by the total number of significant interactions to get the proportion of significant interactions hit by the reads in this subsample
+	return float(hit/2)/float(sig_ints) #Divide the number of hit significant interactions by two, as they were added "twice" to the set, then divide by the total number of significant interactions to get the proportion of significant interactions hit by the reads in this subsample"""
 
 def search_bins(bins,start,end,pos):
-	bin_index = end+((start-end)/2)
+	bin_index = start+((end-start)/2)
 	bin = bins[bin_index].split(',')
 	if pos >= bin[0] and pos <= bin[1]:
 		result = [bin_index]
@@ -122,5 +197,7 @@ parser = argparse.ArgumentParser(description="")
 parser.add_argument("-p","--probe_list_fp")
 parser.add_argument("-i","--sig_ints_fp")
 parser.add_argument("-s","--sam_file_fp")
+parser.add_argument("-n","--num_iter",help="The number of subsamples to generate for each step")
+parser.add_argument("-k","--keep_subsample_files",help="Do not delete subsample files (default: False)",action="store_true",default="False")
 args = parser.parse_args()
 main(args)
